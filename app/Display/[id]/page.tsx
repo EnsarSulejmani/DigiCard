5faa18b78a9a2f60184bcbce4052d97154bfc025
SuatDauti <sd30163@seeu.edu.mk>
2024-05-24 19:01:48 +0200

    Router
    Creating the routing for the application

---
+++ app/Display/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DisplayID = () => {
+  return <div>DisplayID</div>;
+};
+
+export default DisplayID;
---
+++ app/Editor/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const EditorID = () => {
+  return <div>EditorID</div>;
+};
+
+export default EditorID;
