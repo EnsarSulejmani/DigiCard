2b8da680d0b0845b3f69f1144703eb649bcc7534
Ensar Sulejmani <149506033+EnsarSulejmani@users.noreply.github.com>
2024-05-26 10:43:44 +0200

    Navbar Responsive

--- app/Components/Navbar.tsx
+++ app/Components/Navbar.tsx
@@ -1,49 +1,89 @@
 "use client";
 
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import ReUsableButton from "./ReUsableButton";
 import { useSession, signOut } from "next-auth/react";
 
 const Navbar = () => {
   const { data: session } = useSession();
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
 
   return (
     <div className="border-b-2 border-prod flex w-full justify-center flex-wrap">
-      <div className="max-w-[960px] w-[900px] flex h-full items-center justify-between mr-24">
-        <div className="flex justify-between w-full h-full align-middle flex-wrap">
+      <div className="max-w-[960px] w-full flex h-full items-center justify-between px-4 md:px-0">
+        <div className="flex justify-between w-full h-full items-center lg:pr-12">
           <Link href="/" passHref>
             <img src="/Logo Figma SVG.svg" width={150} className="my-4"></img>
           </Link>
-          <div className="flex items-center gap-4">
+          <div className="md:hidden">
+            <button
+              onClick={toggleMobileMenu}
+              className="text-gray-500 hover:text-gray-700 focus:outline-none focus:text-gray-700"
+            >
+              <svg
+                className="h-6 w-6"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {isMobileMenuOpen ? (
+                  <path d="M6 18L18 6M6 6l12 12" />
+                ) : (
+                  <path d="M4 6h16M4 12h16M4 18h16" />
+                )}
+              </svg>
+            </button>
+          </div>
+          <div
+            className={`${
+              isMobileMenuOpen ? "block" : "hidden"
+            } md:flex md:items-center md:gap-4 w-full md:w-auto`}
+          >
             <Link
               href="/"
-              className="transition ease-in-out hover:bg-prod py-5"
+              className="transition ease-in-out hover:bg-prod py-5 block md:inline-block"
             >
               Home
             </Link>
             <Link
               href="/"
-              className="transition ease-in-out hover:bg-prod py-5"
+              className="transition ease-in-out hover:bg-prod py-5 block md:inline-block"
             >
               About
             </Link>
             <Link
               href={`/Dashboard/${session?.user?.name}/123`}
-              className="transition ease-in-out hover:bg-prod py-5"
+              className="transition ease-in-out hover:bg-prod py-5 block md:inline-block"
             >
               Dashboard
             </Link>
             <Link
               href="/"
-              className="transition ease-in-out hover:bg-prod py-5"
+              className="transition ease-in-out hover:bg-prod py-5 block md:inline-block"
             >
               Contact
             </Link>
           </div>
         </div>
+        <div className="hidden md:flex items-center justify-end">
+          {session ? (
+            <ReUsableButton onClick={() => signOut()}>Log Out</ReUsableButton>
+          ) : (
+            <Link href="/Login" className="underline hover:text-prod" passHref>
+              <ReUsableButton>Login</ReUsableButton>
+            </Link>
+          )}
+        </div>
       </div>
-      <div className="flex items-center justify-end">
+      <div className="flex md:hidden items-center justify-end w-full px-4 pb-4">
         {session ? (
           <ReUsableButton onClick={() => signOut()}>Log Out</ReUsableButton>
         ) : (
--- app/Display/[id]/page.tsx
+++ app/Display/[id]/page.tsx
@@ -4,18 +4,23 @@ import React from "react";
 const Display = () => {
   return (
     // Full Container
-    <div className="w-full h-[91vh] bg-blackBackground flex flex-col justify-around">
+    <div className="w-full min-h-screen bg-blackBackground flex flex-col justify-around p-4">
       {/* Logo */}
       <div className="w-full flex justify-center items-center my-8">
-        <img className="" src="/Logo Figma SVG.svg" width={400}></img>
+        <img
+          className="max-w-full h-auto"
+          src="/Logo Figma SVG.svg"
+          alt="Logo"
+          width={400}
+        ></img>
       </div>
 
       {/* Paragraph */}
-      <div className="w-full flex justify-around items-center my-8 gap-8 flex-col">
+      <div className="w-full flex flex-col justify-around items-center my-8 gap-8">
         {/* To Map */}
-        <div className="w-full flex justify-around items-center gap-8 flex-col">
-          <h1 className="text-prod text-2xl">Why work with us?</h1>
-          <p className="max-w-[960px] text-center">
+        <div className="w-full flex flex-col justify-around items-center gap-8 px-4 md:px-0">
+          <h1 className="text-prod text-2xl text-center">Why work with us?</h1>
+          <p className="max-w-[960px] text-center text-white">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
             egestas ante eu lacus egestas tristique. Ut sollicitudin mauris vel
             purus interdum malesuada. Vestibulum mauris sem, vestibulum ac
@@ -26,11 +31,11 @@ const Display = () => {
       </div>
 
       {/* Links */}
-      <div className="w-full flex justify-around items-center my-8 flex-wrap">
+      <div className="w-full flex flex-col justify-around items-center my-8 gap-4 px-4 md:px-0">
         {/* To Map */}
-        <div className="flex gap-2">
-          <p>Work Email: </p>
-          <Link className="text-prod" href="https://www.facebook.com/">
+        <div className="flex flex-col md:flex-row gap-2 items-center text-center md:text-left">
+          <p className="text-white">Work Email: </p>
+          <Link className="text-prod" href="mailto:Loremipsum@gmail.com">
             Loremipsum@gmail.com
           </Link>
         </div>
